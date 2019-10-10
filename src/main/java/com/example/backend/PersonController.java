6c1df3e5aa71b58d6b05b688ec91847f5bb0a4f3
Sucheta Krishali <Sucheta.krishali@timesinternet.in>
2019-10-10 15:27:22 +0530

    cross origin changes

--- src/main/java/com/example/backend/PersonController.java
+++ src/main/java/com/example/backend/PersonController.java
@@ -2,11 +2,13 @@ package com.example.backend;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class PersonController {
 
 	@Autowired
